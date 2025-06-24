22cebcd53d4e4ea27ac1a50ed2f8d08905e92373
M. Fadil Martias <64735243+fadilmartias@users.noreply.github.com>
2025-06-24 20:54:44 +0700

    remove password

--- app/responses/user_response.go
+++ app/responses/user_response.go
@@ -3,13 +3,12 @@ package responses
 import "time"
 
 type UserResponse struct {
-	ID *string `json:"id,omitempty"`
-	Name *string `json:"name,omitempty"`
-	Email *string `json:"email,omitempty"`
-	Password *string `json:"password,omitempty"`
-	Role *string `json:"role,omitempty"`
+	ID              *string    `json:"id,omitempty"`
+	Name            *string    `json:"name,omitempty"`
+	Email           *string    `json:"email,omitempty"`
+	Role            *string    `json:"role,omitempty"`
 	EmailVerifiedAt *time.Time `json:"email_verified_at,omitempty"`
-	CreatedAt *time.Time `json:"created_at,omitempty"`
-	UpdatedAt *time.Time `json:"updated_at,omitempty"`
-	DeletedAt *time.Time `json:"deleted_at,omitempty"`
+	CreatedAt       *time.Time `json:"created_at,omitempty"`
+	UpdatedAt       *time.Time `json:"updated_at,omitempty"`
+	DeletedAt       *time.Time `json:"deleted_at,omitempty"`
 }
